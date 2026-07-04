7f3e7b58049e02ed6c38978ab1b9bd0aab7d34c2
saifulmiraz <saifulislam.miraz11@gmail.com>
2026-07-04 06:12:31 +0000

    remove solutions section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,6 @@
 import Nav from "@/components/sections/Nav";
 import Hero from "@/components/sections/Hero";
 import Stats from "@/components/sections/Stats";
-import Solutions from "@/components/sections/Solutions";
 import Projects from "@/components/sections/Projects";
 import Services from "@/components/sections/Services";
 import Pricing from "@/components/sections/Pricing";
@@ -18,7 +17,6 @@ export default function Home() {
       <main id="main">
         <Hero />
         <Stats />
-        <Solutions />
         <Projects />
         <Services />
         <Pricing />
